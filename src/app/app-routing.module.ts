2687f015aff9acf40784d902c2e5a0c73c39ca2e
Prasanna Raja <prasannaraja@msn.com>
2021-11-17 16:12:28 +0100

    lazy loading route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+
+// const routes: Routes = [
+//   { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: 'home', component: HomeComponent },
+//   { path: '**', component: PageNotFoundComponent },
+// ];
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: '**', component: PageNotFoundComponent },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', loadChildren: () => HomeComponent }
+    ],
+  },
 ];
 
 @NgModule({
